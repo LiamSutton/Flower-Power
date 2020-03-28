be0936e78ac9fdc7bd90172c50fc798c55914c6f
Liam <liamsutton1997@outlook.com>
2020-03-28 10:41:05 +0000

    Seeds now render in FlowerBed

--- src/com/liam/FlowerBed.java
+++ src/com/liam/FlowerBed.java
@@ -5,18 +5,31 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
+
 public class FlowerBed extends GameObject {
     GraphicsContext foregroundContext;
+    ArrayList<Growable> flowers;
 
     public FlowerBed(GraphicsContext gc, GraphicsContext foregroundContext, double x, double y, int xSize, int ySize) {
         super(gc, x, y, xSize, ySize);
         img = new Image("/Flowerbed.png");
         this.foregroundContext = foregroundContext;
+        flowers = new ArrayList<Growable>();
+        plantSeeds(8);
         update();
     }
 
     @Override
     public void update() {
         super.update();
+    }
+
+    public void plantSeeds(int amount) {
+        for (int i = 0; i < amount; i++) {
+            double x = this.x + 128 * i;
+            double y = this.y - 96;
+            flowers.add(new Seed(foregroundContext, x, y, 128, 128));
+        }
     }
 }
