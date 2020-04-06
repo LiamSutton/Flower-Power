465105c474e266f33735bfc75ace7d4b6abbacec
Liam Sutton <liamsutton1997@outlook.com>
2020-04-06 15:17:28 +0100

    Refactored Sun class and documented it

--- src/com/liam/Sun.java
+++ src/com/liam/Sun.java
@@ -3,24 +3,40 @@ package com.liam;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
 
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.concurrent.Flow;
-
+/**
+ * This class represents a Sun Object the player controls to grow the flowers
+ */
 public class Sun extends GameObject implements IMoveable {
-    int currentPosition = 0;
-    final int[] yPositions = {224, 128, 32, 0, 0, 32, 128, 224};
+    /**
+     * The current position of the Sun on the grid
+     */
+    int currentPosition;
+
+    /**
+     * A fixed array of Y co-ordinates representing the Suns position when on the relevant position of the grid
+     */
+    final int[] yPositions = {224, 128, 32, 0, 0, 32, 128, 224}; // Quicker than calculating on the fly
+
+    /**
+     * Instantiates the Object and sets its grid position to the 0th index
+     * @param gc The context provided by the canvas (layer) it resides on
+     * @param x The X position of the Object
+     * @param y the Y position of the Object
+     */
     public Sun(GraphicsContext gc, double x, double y) {
         super(gc, x, y);
         img = new Image(Constants.SUN);
         super.setSizeProperties();
+        currentPosition = 0;
         update();
     }
-
+    /**
+     * This function will move the Sun 1 place in the direction specified, if the move would take the Sun
+     * off of the screen, it will loop to the opposite side of the grid
+     * @param direction The value the users input maps to
+     */
+    // TODO: replace 128 with constant
     @Override
     public void move(int direction) {
         if (direction == Constants.DIRECTION_RIGHT) {
@@ -42,9 +58,28 @@ public class Sun extends GameObject implements IMoveable {
 
     }
 
+
+    /**
+     *  Draws the Sun and its Ray
+     */
     @Override
     public void update() {
         super.update();
+        castRay();
+    }
+
+    /**
+     * Attempts to grow the Object represented by the delegate the Object represents
+     * @param target The Object representing the delegate
+     */
+    public void Shine(FlowerDelegator target) {
+        target.grow();
+    }
+
+    /**
+     * Casts a ray to indicate the flower that is currently getting grown by the Sun
+     */
+    public void castRay() {
         gc.setGlobalAlpha(0.2);
         gc.setFill(Color.DARKORANGE);
         gc.setStroke(Color.YELLOW);
@@ -52,11 +87,5 @@ public class Sun extends GameObject implements IMoveable {
 
         gc.fillPolygon(new double[]{x, x+64, x+128}, new double[]{700, y+128, 700}, 3);
         gc.setGlobalAlpha(1);
-    }
-
-    public void Shine(FlowerDelegator target) {
-
-        target.grow();
-
     }
 }
