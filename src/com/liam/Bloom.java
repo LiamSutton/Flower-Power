63f9b46a90216899a7c53ab660f636bf46d86798
Liam Sutton <liamsutton1997@outlook.com>
2020-04-06 15:47:27 +0100

    Finished documenting IFlower Objects

--- src/com/liam/Bloom.java
+++ src/com/liam/Bloom.java
@@ -3,24 +3,40 @@ package com.liam;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+/**
+ * Represents a Bloom Object which is the 5th and final stage of flower growth
+ */
 public class Bloom extends Growable implements IFlower {
-    public Bloom(GraphicsContext gc, double x, double y, double h) {
+    /**
+     * Instantiates the Object and sets its current health to the value of passedHealth
+     * @param gc The context provided by the canvas (layer) the Object resides on
+     * @param x The X position of the Bloom
+     * @param y the Y position of the Bloom
+     * @param passedHealth the heath value of the ancestor Object
+     */
+    public Bloom(GraphicsContext gc, double x, double y, double passedHealth) {
         super(gc, x, y);
         healthy = Constants.BLOOM_HEALTHY;
         wilted = Constants.BLOOM_WILTED;
         img = new Image(healthy);
         setSizeProperties();
         maxHealth = 200;
-        currentHealth = h;
+        currentHealth = passedHealth;
         update();
     }
 
+    /**
+     * See IFlower interface
+     */
     @Override
     public void heal() {
         currentHealth += Constants.BLOOM_HEAL_VALUE;
         clampHealth();
     }
 
+    /**
+     * See IFlower interface
+     */
     @Override
     public void wilt() {
         currentHealth -= Constants.BLOOM_WILT_RATE;
--- src/com/liam/Blossom.java
+++ src/com/liam/Blossom.java
@@ -3,18 +3,31 @@ package com.liam;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+/**
+ * Represents a Blossom Object which is the 4th stage of flower growth
+ */
 public class Blossom extends Growable implements IFlower {
-    public Blossom(GraphicsContext gc, double x, double y, double h) {
+    /**
+     * Instantiates the Blossom Object and sets its current health to the value of passedHealth
+     * @param gc The context provided by the canvas (layer) the Object resides on
+     * @param x The X position of the Blossom
+     * @param y the Y position of the Blossom
+     * @param passedHealth the health value passed from the ancestor Object
+     */
+    public Blossom(GraphicsContext gc, double x, double y, double passedHealth) {
         super(gc, x, y);
         healthy = Constants.BLOSSOM_HEALTHY;
         wilted = Constants.BLOSSOM_WILTED;
         img = new Image(healthy);
         setSizeProperties();
         maxHealth = 175;
-        currentHealth = h;
+        currentHealth = passedHealth;
         update();
     }
 
+    /**
+     * See IFlower interface
+     */
     @Override
     public void heal() {
         currentHealth += Constants.BLOSSOM_HEAL_VALUE;
@@ -22,7 +35,9 @@ public class Blossom extends Growable implements IFlower {
     }
 
 
-
+    /**
+     * See IFlower interface
+     */
     @Override
     public void wilt() {
         currentHealth -= Constants.BLOSSOM_WILT_RATE;
