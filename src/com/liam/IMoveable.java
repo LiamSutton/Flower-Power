6098c8ba2e410b9d051adc5a5f7a077a0e830e7f
Liam Sutton <liamsutton1997@outlook.com>
2020-04-06 15:07:55 +0100

    Documented the IMoveable interface

--- src/com/liam/IMoveable.java
+++ src/com/liam/IMoveable.java
@@ -1,5 +1,13 @@
 package com.liam;
 
+/**
+ * This interface is implemented by all Objects which are controlled by the player
+ */
 public interface IMoveable {
+
+    /**
+     * This function will move an Object that implements the Interface in a given direction
+     * @param direction The direction the Players input maps to
+     */
     void move(int direction);
 }
